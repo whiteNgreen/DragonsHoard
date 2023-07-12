184aabd95f05f8f2ee013a7629f1acf66e44fd9d
Adrian Stræte <adrian.r.strate@gmail.com>
2023-07-12 16:50:26 +0200

    Initial project
    Just to get started with prototyping and getting a project up and running.

---
+++ Source/DragonsHoard/DragonsHoard.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DragonsHoard.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DragonsHoard, "DragonsHoard" );
